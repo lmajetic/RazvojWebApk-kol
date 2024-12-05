a9f69eeb236540e2213cdd0e7880f397dae02c4d
lmajetic <lukamajeticc@gmail.com>
2024-12-05 11:02:06 +0100

    Dodavanje quasar projekta i readme datoteke.

---
+++ BookShop/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>5.12.2024. Luka Majetić</h3>
+    
+    
+  </q-page>
+  
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
